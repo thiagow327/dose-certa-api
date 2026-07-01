b326c42dd1eeef71f6eea3c2586c357a53b99c54
Thiago Gomes <thiagosilva327@live.com>
2026-06-30 23:56:40 -0300

    fix: remove dependecia de app para criar o engine, session e o base

--- database/db.py
+++ database/db.py
@@ -1,3 +1,8 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-db = SQLAlchemy()
+Base = declarative_base()
+
+engine = create_engine("sqlite:///database/dose_certa.db")
+Session = sessionmaker(bind=engine)
